0d52542e5b4cc284adef272939b8eb92e12a4791
LuizGustavoP <62386766+LuizGustavoP@users.noreply.github.com>
2021-11-07 17:45:38 -0300

    Expande testes e adiciona lib para stubs

--- Assets/Tests/Editmode/TestImageSpawner.cs
+++ Assets/Tests/Editmode/TestImageSpawner.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.TestTools;
+using NSubstitute;
 
 public class TestImageSpawner
 {
@@ -11,6 +12,35 @@ public class TestImageSpawner
     {
         ImageSpawner imageSpawner = CreateImageSpawner();
 
+
+        yield return null;
+    }
+
+    [UnityTest]
+    public IEnumerator LoadTextures()
+    {
+        ImageSpawner imageSpawner = CreateImageSpawner();
+        
+        {
+            var stub = Substitute.For<TextureLoaderInterface>();
+            stub.LoadTextures().Returns(x => null);
+
+            imageSpawner.TestingSetterTextureLoaderInterface(stub);
+            Object[] textures = imageSpawner.LoadTextures();
+
+            Assert.AreEqual(1, textures.Length);
+        }
+
+        {
+            Object[] expectedTextures = new Object[5];
+            var stub = Substitute.For<TextureLoaderInterface>();
+            stub.LoadTextures().Returns(x => expectedTextures);
+
+            imageSpawner.TestingSetterTextureLoaderInterface(stub);
+            Object[] textures = imageSpawner.LoadTextures();
+
+            Assert.AreEqual(expectedTextures, textures);
+        }
 
         yield return null;
     }
@@ -29,7 +59,6 @@ public class TestImageSpawner
     {
         ImageSpawner imageSpawner = CreateImageSpawner();
 
-
         yield return null;
     }
 
@@ -37,6 +66,7 @@ public class TestImageSpawner
     public IEnumerator GetSpawnRow() //Realizar depois
     {
         ImageSpawner imageSpawner = CreateImageSpawner();
+
 
 
         yield return null;
@@ -74,6 +104,23 @@ public class TestImageSpawner
     {
         ImageSpawner imageSpawner = CreateImageSpawner();
 
+        Quaternion rot = imageSpawner.GetSpawnRotation(0);
+        Assert.AreEqual(
+            Quaternion.LookRotation(
+               new Vector3(1.0f, 0.0f, 0.0f),
+               new Vector3(0.0f, -1.0f, 0.0f)
+            ),
+            rot
+        );
+        
+        rot = imageSpawner.GetSpawnRotation(5);
+        Assert.AreEqual(
+            Quaternion.LookRotation(
+                new Vector3(0.0f, 0.0f, 1.0f),
+                new Vector3(0.0f, -1.0f, 0.0f)
+            ),
+            rot
+        );
 
         yield return null;
     }
@@ -83,6 +130,17 @@ public class TestImageSpawner
     {
         ImageSpawner imageSpawner = CreateImageSpawner();
 
+        for (int i = 0; i < 5; i++)
+        {
+            GameObject child = new GameObject();
+            imageSpawner.SetChildSize(child);
+            Assert.GreaterOrEqual(child.transform.localScale.x, child.transform.localScale.x * 0.5f);
+            Assert.GreaterOrEqual(child.transform.localScale.y, child.transform.localScale.y * 0.5f);
+            Assert.GreaterOrEqual(child.transform.localScale.z, child.transform.localScale.z * 0.5f);
+            Assert.LessOrEqual(child.transform.localScale.x, child.transform.localScale.x * 2.0f);
+            Assert.LessOrEqual(child.transform.localScale.y, child.transform.localScale.y * 2.0f);
+            Assert.LessOrEqual(child.transform.localScale.z, child.transform.localScale.z * 2.0f);
+        }
 
         yield return null;
     }
